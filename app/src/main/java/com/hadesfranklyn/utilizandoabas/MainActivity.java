fb3bcf4492d0c0ad914d02d4c1b23d864b5b3d80
Franklyn R <franklyn1998@live.com>
2022-09-05 18:58:45 -0300

    Utilizando Abas

--- app/src/main/java/com/hadesfranklyn/utilizandoabas/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/utilizandoabas/MainActivity.java
@@ -1,14 +1,41 @@
 package com.hadesfranklyn.utilizandoabas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.hadesfranklyn.utilizandoabas.fragment.EmAltaFragment;
+import com.hadesfranklyn.utilizandoabas.fragment.HomeFragment;
+import com.hadesfranklyn.utilizandoabas.fragment.InscricoesFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        // Configurar adapter para abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Home", HomeFragment.class)
+                .add("InscricoesFragment", InscricoesFragment.class)
+                .add("EmAltaFragment", EmAltaFragment.class)
+                .create());
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
     }
 }
